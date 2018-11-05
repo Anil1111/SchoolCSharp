0b8433234a6f4bf613f8f1cefd137a9f19d7febb
mHawk <italia195@outlook.it>
2018-11-05 21:42:35 +0100

    Improved algorithm performance and usability.

--- Projects/Converter.Core/BaseConverter.cs
+++ Projects/Converter.Core/BaseConverter.cs
@@ -5,63 +5,27 @@ namespace Converter.Core
     public static class BaseConverter
     {
         /// <summary>
-        /// Multiplies every number by <see cref="@base"/> elevated at position
+        /// Converts any number to the specified base rappresentation.
         /// </summary>
-        /// <param name="num">Input in base <see cref="@base"/></param>
-        /// <param name="base">Number base</param>
-        /// <returns>Base 10 representation of <see cref="num"/></returns>
-        /// <exception cref="ArgumentException">Cannot convert bases over 10</exception>
-        public static int ToBase10(int num, int @base)
+        /// <param name="num">Number to convert to base <i><see cref="toBase"/></i></param>
+        /// <param name="fromBase">Original base of <i><see cref="num"/></i></param>
+        /// <param name="toBase">Base the number gets converted to</param>
+        /// <returns>Number represented in base <i><see cref="toBase"/></i></returns>
+        /// <exception cref="ArgumentException">Bases must be within 2 and 10</exception>
+        public static ulong Convert(ulong num, ulong fromBase, ulong toBase)
         {
-            return Convert(num, @base, 10);
-//            if (@base > 10)
-//                throw new ArgumentException("Cannot convert base {0}: Cannot rappresent digits with letters.");
-//            
-//            var output = 0;
-//            var length = (int) Math.Log10(num) + 1;
-//            for (int i = length - 1; i >= 0; i--)
-//            {
-//                var numAtPos = num / (int) Math.Pow(10, i) % 10;
-//                output += numAtPos * (int) Math.Pow(@base, i);
-//            }
-//            return output;
-        }
+            if (fromBase < 2 || fromBase > 10 ||
+                toBase < 2 || toBase > 10)
+                throw new ArgumentException($"Cannot convert from base {fromBase} to base {toBase}: Bases must be within 2 and 10");
 
-        /// <summary>
-        /// Divides every number by <see cref="@base"/> and writes the reminder.
-        /// </summary>
-        /// <param name="base10">Number to convert in base 10</param>
-        /// <param name="base">Base to convert to</param>
-        /// <returns>Number represented in the specified base</returns>
-        /// <exception cref="ArgumentException">Cannot convert bases over 10</exception>
-        public static int ToBase(int base10, int @base)
-        {
-            return Convert(base10, 10, @base);
-//            if (@base > 10)
-//                throw new ArgumentException("Cannot convert base {0}: Cannot represent digits with letters.");
-//            
-//            var output = 0;
-//            int times = (int) Math.Log(base10, @base) + 1;
-//            for (int i = times - 1; i >= 0; i--)
-//            {
-//                var num = base10 / (int) Math.Pow(@base, i) % @base;
-//                output += num * (int) Math.Pow(10, i);
-//            }
-//            return output;
-        }
-
-        private static int Convert(int num, int fromBase, int toBase)
-        {
-            if (fromBase > 10 || toBase > 10)
-                throw new ArgumentException("Cannot convert base {0}: Cannot represent digits with letters.");
-            
-            var output = 0;
-            int times = (int) Math.Log(num, toBase) + 1;
-            for (int i = times - 1; i >= 0; i--)
+            ulong output = 0;
+            var weight = fromBase;
+            while (num > 0)
             {
-                var a = num / (int) Math.Pow(toBase, i) % toBase;
-                output += a * (int) Math.Pow(fromBase, i);
+                output += (num /= toBase) % toBase * weight;
+                weight *= fromBase;
             }
+            
             return output;
         }
     }
