7cef846afc184cb5c0111e5857a012db9f74e0a4
mHawk <italia195@outlook.it>
2018-11-06 19:13:09 +0100

    Improved TextBox validating and changed algorithm to the string-based one.

--- Projects/Converter.Interface/MainForm.cs
+++ Projects/Converter.Interface/MainForm.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using Converter.Core;
 
@@ -20,58 +15,53 @@ namespace Converter.Interface
 
         private void OnLeftToRight(object sender, EventArgs e)
         {
-            if (!ulong.TryParse(txtNumberLeft.Text, out ulong num))
-                return;
-
             ulong fromBase = (ulong)fieldBaseLeft.Value;
             ulong toBase = (ulong)fieldBaseRight.Value;
-
-            txtNumberRight.Text = BaseConverter.Convert(num, fromBase, toBase).ToString();
+            
+            txtNumberRight.Text = BaseConverter.Convert(txtNumberLeft.Text, fromBase, toBase);
         }
 
         private void OnRightToLeft(object sender, EventArgs e)
         {
-            if (!ulong.TryParse(txtNumberRight.Text, out ulong num))
-                return;
-
             ulong fromBase = (ulong)fieldBaseRight.Value;
             ulong toBase = (ulong)fieldBaseLeft.Value;
 
-            txtNumberLeft.Text = BaseConverter.Convert(num, fromBase, toBase).ToString();
+            txtNumberLeft.Text = BaseConverter.Convert(txtNumberRight.Text, fromBase, toBase);
         }
 
         private void OnValidatingLeft(object sender, EventArgs e)
         {
-            var @base = (ulong)fieldBaseLeft.Value;
-            var valid = ulong.TryParse(txtNumberLeft.Text, out ulong num);
-
-            while (valid && num > 0)
-            {
-                if (num % 10 >= @base)
-                    valid = false;
-                
-                num /= 10;
-            }
-
+            var @base = (int)fieldBaseLeft.Value;
+            var valid = !string.IsNullOrEmpty(txtNumberLeft.Text) &&
+                        txtNumberLeft.Text.All(x => IsValid(x, @base));
+            
             txtNumberLeft.ForeColor = valid ? Color.Black : Color.Red;
             btnLeftToRight.Enabled = valid;
         }
         
         private void OnValidatingRight(object sender, EventArgs e)
         {
-            var @base = (ulong)fieldBaseRight.Value;
-            var valid = ulong.TryParse(txtNumberRight.Text, out ulong num);
-
-            while (valid && num > 0)
-            {
-                if (num % 10 >= @base)
-                    valid = false;
-                
-                num /= 10;
-            }
-
+            var @base = (int)fieldBaseRight.Value;
+            var valid = !string.IsNullOrEmpty(fieldBaseRight.Text) &&
+                        txtNumberRight.Text.All(x => IsValid(x, @base));
+            
             txtNumberRight.ForeColor = valid ? Color.Black : Color.Red;
             btnRightToLeft.Enabled = valid;
+        }
+
+        private static bool IsValid(char c, int @base)
+        {
+            // Ascii Values
+            // a  97 0x61 | z 122 0x7A | 122 - 97 = 25
+            // A  65 0x41 | Z  90 0x5A |  90 - 65 = 25
+            // 0  48 0x30 | 9  57 0x39 |  57 - 48 = 9
+            if (c >= 48 && c <= 57) // If Digit
+                return c - 48 < @base; // Check if digit is less than @base
+            if (c >= 97 && c <= 122) // If UpperCase
+                return c - 87 < @base; // Check if char value (from 'a' to c) is less than @base
+            if (c >= 65 && c <= 90) // If LowerCase
+                return c - 55 < @base; // Check if char value (from 'A' to c) is less than @base
+            return false;
         }
     }
 }
