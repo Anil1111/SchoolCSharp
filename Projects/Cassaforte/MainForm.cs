797eb1694548457b9b0ea9f0ddb3a76eea9261f9
mHawk <italia195@outlook.it>
2018-11-18 13:24:03 +0100

    Documented code

--- Projects/Cassaforte/MainForm.cs
+++ Projects/Cassaforte/MainForm.cs
@@ -7,32 +7,64 @@ namespace Cassaforte
 {
     public partial class MainForm : Form
     {
+        /// <summary>
+        /// Default <see cref="TextBoxBase.MaxLength"/> of <see cref="TextBox"/> object
+        /// </summary>
         private const int DefaultMaxLength = 32767;
+        
+        /// <summary>
+        /// The correct password
+        /// </summary>
         private string _password = string.Empty;
+        
+        /// <summary>
+        /// The number of tries remaining before the locker locks itself
+        /// </summary>
         private int _tries = 3;
-        private int _index;
+        
+        /// <summary>
+        /// The length of the string
+        /// </summary>
+        private int _length;
 
         public MainForm()
         {
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Called when we click whichever numeric button
+        /// </summary>
+        /// <param name="sender">Object who fired this event</param>
+        /// <param name="e">Event arguments</param>
         private void OnDigit(object sender, EventArgs e)
         {
+            // Get the string contained in the button (1-9)
             var number = ((Button) sender).Text; // Can be achieved using 9 methods.
-            if (_index == 0)
+            
+            // Delete the message currently displaying
+            if (_length == 0)
                 txtInputOutput.Text = string.Empty;
             
-            if (_index < 5)
+            // While the _length is less than 5 (we didn't reach password length)
+            if (_length < 5)
             {
+                // Enable password character replacement
                 txtInputOutput.UseSystemPasswordChar = true;
+                // Append the clicked button text to the password guess
                 txtInputOutput.Text += number;
-                _index++;
-                if (_index == 5)
+                // Increment the current string length
+                _length++;
+                
+                if (_length == 5)
                 {
+                    // Disable password character replacement in order to show output messages
                     txtInputOutput.UseSystemPasswordChar = false;
+                    
+                    // Verifies whether the guessed password is correct; Same as _password == txtInputOutput.Text 
                     if (string.Equals(_password, txtInputOutput.Text, StringComparison.Ordinal))
                     {
+                        // If they are equal, display "Cassaforte aperta" and prevent any more input to be registered.
                         txtInputOutput.Text = "Cassaforte aperta";
                         btnNum1.Enabled = false;
                         btnNum2.Enabled = false;
@@ -46,9 +78,11 @@ namespace Cassaforte
                     }
                     else
                     {
+                        // Decrement the number of tries remaining
                         _tries--;
                         if (_tries <= 0)
                         {
+                            // If no tries are left, display "Cassaforte bloccata" and prevent any more input to be registered.
                             txtInputOutput.Text = "Cassaforte bloccata";
                             btnNum1.Enabled = false;
                             btnNum2.Enabled = false;
@@ -62,19 +96,28 @@ namespace Cassaforte
                         }
                         else
                         {
+                            // If _tries > 0, display the number of tries remaining
                             txtInputOutput.Text = $"Errore, hai ancora {_tries} tentativi";
                         }
                     }
                     
-                    _index = 0;
+                    // Reset the password guess length to 0
+                    _length = 0;
                 }
             }
         }
 
+        /// <summary>
+        /// Called when we left click on the TextBox
+        /// </summary>
+        /// <param name="sender">Object who fired this event</param>
+        /// <param name="e">Event arguments</param>
         private void OnTextboxSelected(object sender, MouseEventArgs e)
         {
+            // If box is not readonly
             if (!txtInputOutput.ReadOnly)
             {
+                // Prepare to receive the right password as input
                 txtInputOutput.Text = string.Empty;
                 txtInputOutput.TextAlign = HorizontalAlignment.Left;
                 txtInputOutput.MaxLength = 5;
@@ -82,6 +125,11 @@ namespace Cassaforte
             }
         }
 
+        /// <summary>
+        /// Validates the TextBox content
+        /// </summary>
+        /// <param name="sender">Object who fired this event</param>
+        /// <param name="e">Event arguments</param>
         private void OnValidatingTextbox(object sender, CancelEventArgs e)
         {
             if (txtInputOutput.ReadOnly)
@@ -90,16 +138,27 @@ namespace Cassaforte
             e.Cancel = txtInputOutput.TextLength < 5 || !Regex.IsMatch(txtInputOutput.Text, @"[1-9]{5}");
         }
 
+        /// <summary>
+        /// Saves the password and enables user to guess it
+        /// </summary>
+        /// <param name="sender">Object who fired this event</param>
+        /// <param name="e">Event arguments</param>
         private void OnConfirmCombination(object sender, EventArgs e)
         {
+            // Get the password from the TextBox
             _password = txtInputOutput.Text;
+            
+            // Prevents this button from being pressed again
             btnConfirmCombination.Enabled = false;
+            
+            // Makes TextBox output "Inserire la combinazione"
             txtInputOutput.ReadOnly = true;
             txtInputOutput.TextAlign = HorizontalAlignment.Center;
             txtInputOutput.MaxLength = DefaultMaxLength;
             txtInputOutput.Text = "Inserire la combinazione";
             txtInputOutput.UseSystemPasswordChar = false;
 
+            // Enables numeric buttons
             btnNum1.Enabled = true;
             btnNum2.Enabled = true;
             btnNum3.Enabled = true;
