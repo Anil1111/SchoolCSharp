2ddd53c91bf9f2073f63ffbc18bf23c0852b925c
mHawk <italia195@outlook.it>
2018-11-29 13:20:26 +0100

    Added WordSplitter project

---
+++ Projects/WordSplitter/MainForm.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace WordSplitter
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.txtInputText = new System.Windows.Forms.RichTextBox();
+            this.btnCompute = new System.Windows.Forms.Button();
+            this.lsbWords = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // txtInputText
+            // 
+            this.txtInputText.Location = new System.Drawing.Point(12, 12);
+            this.txtInputText.Name = "txtInputText";
+            this.txtInputText.Size = new System.Drawing.Size(668, 60);
+            this.txtInputText.TabIndex = 0;
+            this.txtInputText.Text = resources.GetString("txtInputText.Text");
+            // 
+            // btnCompute
+            // 
+            this.btnCompute.Location = new System.Drawing.Point(686, 12);
+            this.btnCompute.Name = "btnCompute";
+            this.btnCompute.Size = new System.Drawing.Size(86, 60);
+            this.btnCompute.TabIndex = 1;
+            this.btnCompute.Text = "Separa";
+            this.btnCompute.UseVisualStyleBackColor = true;
+            this.btnCompute.Click += new System.EventHandler(this.OnCompute);
+            // 
+            // lsbWords
+            // 
+            this.lsbWords.FormattingEnabled = true;
+            this.lsbWords.Location = new System.Drawing.Point(12, 78);
+            this.lsbWords.Name = "lsbWords";
+            this.lsbWords.Size = new System.Drawing.Size(760, 368);
+            this.lsbWords.TabIndex = 2;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.lsbWords);
+            this.Controls.Add(this.btnCompute);
+            this.Controls.Add(this.txtInputText);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.Text = "WordSplitter";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox txtInputText;
+        private System.Windows.Forms.Button btnCompute;
+        private System.Windows.Forms.ListBox lsbWords;
+    }
+}
+
---
+++ Projects/WordSplitter/MainForm.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Forms;
+
+namespace WordSplitter
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Fetches all the words on the input text and adds to them to a list
+        /// </summary>
+        /// <param name="sender">The button pressed</param>
+        /// <param name="e">The event arguments</param>
+        private void OnCompute(object sender, EventArgs e)
+        {
+            // Clears the elements already present in the list
+            lsbWords.Items.Clear();
+            
+            // Gets the input text
+            var text = txtInputText.Text;
+            
+            // Creates a variable that will hold the current word we are currently in
+            string word = string.Empty; // Would have used StringBuilder
+            // Cycles though every character in the text
+            for (int i = 0; i < text.Length; i++)
+            {
+                // Gets the lowercase variant of the current character 
+                var c = char.ToLower(text[i], CultureInfo.CurrentCulture); // We use current culture because we want to keep every language-specific data
+
+                // If it's a letter and it's last, add the letter to word and print word
+                if (i + 1 == text.Length && char.IsLetter(c))
+                {
+                    // Adds the letter to the current word
+                    word += c;
+                    
+                    // We add the word to the list
+                    lsbWords.Items.Add($"{lsbWords.Items.Count + 1}) {word}");
+                }
+                // If it's just a letter add the letter to word
+                else if (char.IsLetter(c))
+                {
+                    // Adds the letter to the current word
+                    word += c;
+                }
+                // If it not a letter print the current word
+                else if (word.Length > 0)
+                {
+                    // We add the word to the list
+                    lsbWords.Items.Add($"{lsbWords.Items.Count + 1}) {word}");
+                    // We start another word
+                    word = string.Empty;
+                }
+            }
+        }
+    }
+}
